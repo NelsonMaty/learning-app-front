4cb60338c01a0f19ffb37e60291b5c7a875d97c2
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-08-07 17:00:45 -0300

    fix: update Gallery page title to 'Learning app'

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -16,7 +16,7 @@ export default function GalleryPage(): ReactElement {
 
 	return (
 		<>
-			<Head title='Vitamin' />
+			<Head title='Learning app' />
 			<div className='m-2 grid min-h-screen grid-cols-[minmax(0,384px)] place-content-center gap-2 md:m-0 md:grid-cols-[repeat(2,minmax(0,384px))] xl:grid-cols-[repeat(3,384px)]'>
 				{data.map((fruit, index) => (
 					<Fruit key={`FruitCard-${fruit.name}`} fruit={fruit} index={index} />
